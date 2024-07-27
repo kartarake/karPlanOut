8a85ccdfa4dd003c3a811c873b708aadf68c5fc8
kartarake <kar.prahveen@gmail.com>
2024-07-27 20:51:05 +0530

    Adding fonts!, theme handler start! tiny progress on page!

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from dearpygui import dearpygui as pygui
 
 class app:
     def __init__(self) -> None:
-        self.apptitle = "karPlanOut"
+        # Variable Dashboard
+        self.apptitle = "karPlanOut" # Title of the window
         
-        self.appwidth = 1930
-        self.appheight = 1040
+        self.appwidth = 1930 # Window's width
+        self.appheight = 1040 # Window's height
 
+        # Initializing and setting up stuff!
         self.init_pygui()
+        self.font_load()
         
+        # App pages
         self.page_firstpage()
 
+        # Start the pygui!
         self.start_pygui()
 
     def init_pygui(self) -> None:
@@ -30,10 +35,31 @@ class app:
         pygui.start_dearpygui()
         pygui.destroy_context()
 
+    def font_load(self) -> None:
+        self.fontpack = {}
+        with pygui.font_registry():
+            # General use fonts
+            self.fontpack["heading"] = pygui.add_font(r"assets\fonts\Montserrat\static\Montserrat-ExtraBold.ttf", 24)
+            self.fontpack["body"] = pygui.add_font(r"assets\fonts\Montserrat\static\Montserrat-Light.ttf", 13)
+            
+            # Specific use fonts
+            self.fontpack["firstpage_buttons"] = pygui.add_font(r"assets\fonts\Jura\static\Jura-Bold.ttf", 16)
+
     def page_firstpage(self) -> None:
         with pygui.window(tag=self.apptitle):
-            pygui.add_button(label="Sign in")
-            pygui.add_button(label="Look around as a guest")
+            signin_button = pygui.add_button(
+                label="Sign in",
+                width=600,
+                height=50
+            )
+            pygui.bind_item_font(signin_button, self.fontpack["firstpage_buttons"])
+
+            guest_button = pygui.add_button(
+                label="Look around as a guest",
+                width=600,
+                height=50,
+            )
+            pygui.bind_item_font(guest_button, self.fontpack["firstpage_buttons"])
 
 if __name__ == "__main__":
     app()
